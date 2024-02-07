77033f42fc760372c94a6665a7733aa5c42adc77
unknown <bryandang10@gmail.com>
2024-02-07 12:30:30 -0800

    add logout function

--- src/utilities/Header.tsx
+++ src/utilities/Header.tsx
@@ -15,6 +15,10 @@ export default function Header() {
     navigate('/');
   }
 
+  const logOut = () => {
+    // Needs to be implemented
+  }
+
   return (
     <header className="flex justify-between items-center py-4 px-8">
       <a href="#" onClick={goToCharacters} className="text-4xl font-pirata custom-yellow-hover-bg">Mausritter Character Sheet</a>
@@ -22,7 +26,7 @@ export default function Header() {
         <ul className="flex space-x-5">
           <li><a href="#" onClick={goToCharacters} className="font-pirata underline custom-yellow-hover-bg">My Characters</a></li>
           <li><a href="#" onClick={goToAbout} className="font-pirata underline custom-yellow-hover-bg">About</a></li>
-          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">Log Out</a></li>
+          <li><a href="#" onClick={logOut} className="font-pirata underline custom-yellow-hover-bg">Log Out</a></li>
         </ul>
       </nav>
     </header>
