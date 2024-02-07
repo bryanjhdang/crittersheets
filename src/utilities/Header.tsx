aff0c7f72cce963f4028188e4788804cd5a93193
unknown <bryandang10@gmail.com>
2024-02-07 12:29:39 -0800

    add navigation from header

--- src/utilities/Header.tsx
+++ src/utilities/Header.tsx
@@ -1,13 +1,27 @@
+import { MouseEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 import './Header.css'
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  const goToAbout = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    navigate('/about');
+  };
+
+  const goToCharacters = (event: MouseEvent<HTMLAnchorElement>) => {
+    event.preventDefault();
+    navigate('/');
+  }
+
   return (
     <header className="flex justify-between items-center py-4 px-8">
-      <h1 className="text-4xl font-pirata custom-yellow-hover-bg">Mausritter Character Sheet</h1>
+      <a href="#" onClick={goToCharacters} className="text-4xl font-pirata custom-yellow-hover-bg">Mausritter Character Sheet</a>
       <nav>
         <ul className="flex space-x-5">
-          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">My Characters</a></li>
-          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">About</a></li>
+          <li><a href="#" onClick={goToCharacters} className="font-pirata underline custom-yellow-hover-bg">My Characters</a></li>
+          <li><a href="#" onClick={goToAbout} className="font-pirata underline custom-yellow-hover-bg">About</a></li>
           <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">Log Out</a></li>
         </ul>
       </nav>
