d2e153e7b7286f55c2cb161ed423141a729a4a12
unknown <bryandang10@gmail.com>
2024-02-07 12:08:08 -0800

    update header with styles

--- src/utilities/Header.tsx
+++ src/utilities/Header.tsx
@@ -2,13 +2,13 @@ import './Header.css'
 
 export default function Header() {
   return (
-    <header className="header">
-      <h1 className="header-title">Mausritter Character Sheet</h1>
-      <nav className="header-nav">
-        <ul>
-          <li><a href="#">My Characters</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Log Out</a></li>
+    <header className="flex justify-between items-center py-4 px-8">
+      <h1 className="text-4xl font-pirata custom-yellow-hover-bg">Mausritter Character Sheet</h1>
+      <nav>
+        <ul className="flex space-x-5">
+          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">My Characters</a></li>
+          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">About</a></li>
+          <li><a href="#" className="font-pirata underline custom-yellow-hover-bg">Log Out</a></li>
         </ul>
       </nav>
     </header>
