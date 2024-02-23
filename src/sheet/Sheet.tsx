4e45f8d0a0505b6b3b777881697d326a7ee7da9c
Joshua Li <JoshuaJLi@outlook.com>
2024-02-23 15:04:23 -0800

    Toss onto remote.

--- src/sheet/Sheet.tsx
+++ src/sheet/Sheet.tsx
@@ -1,5 +1,10 @@
 import { Link } from 'react-router-dom';
 import Header from '../utilities/Header';
+import { stats } from '../classes/sheet/stats';
+
+interface Props {
+    sheet: stats;
+}
 
 function Sheet() {
     return (
@@ -9,6 +14,10 @@ function Sheet() {
                 <p>This is the character sheet component where it loads information based on the info passed in, and you can edit values (needs to take in params)</p>
                 <Link to="/">Go back</Link>
             </header>
+
+            <label htmlFor="name" className='statLabel m-4 p-2 w-8'>Dex</label>
+            <input type='text' id = "name" className='bg-blue-400 outline-black-100' ></input>
+
         </div>
     );
 }
