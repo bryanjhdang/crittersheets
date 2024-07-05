1fc6b4e7e143ab59e9fc2882a54e6ad4cc9e44f0
Bryan <bryandang10@gmail.com>
2024-07-05 12:31:12 -0700

    Restructure client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,13 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import About from "./about/About";
-import Create from "./creation/Create";
-import Sheet from "./sheet/Sheet";
-import Characters from "./characters/Characters";
+
 
 function App() {
   return (
     <Router>
         <Routes>
-            <Route path="/about"
-                element={<About />} />
-            <Route path="/create"
-                element={<Create />} />
-            <Route path="/sheet"
-                element={<Sheet />} />
-            <Route path="/"
-                element={<Characters />} />
+            {/* <Route path="/" */}
+                {/* element={<About />} /> */}
         </Routes>
     </Router>
   );
