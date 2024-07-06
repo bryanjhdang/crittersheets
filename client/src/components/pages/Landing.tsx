38bd70fd1fec5588e09d8f2b1db064ac8ed8a76d
Bryan <bryandang10@gmail.com>
2024-07-05 17:56:45 -0700

    Update Landing text

--- client/src/components/pages/Landing.tsx
+++ client/src/components/pages/Landing.tsx
@@ -28,7 +28,7 @@ const AboutCritterSheets = () => {
 const AboutMausritter = () => {
   return (
     <>
-      <Text>Mausritter is an easy to pick up game where you explore the world as mice adventurers. By using wacky items and magic to find your way, there is a unique physical card-based inventory system reminiscent of Resident Evil to keep things fun.</Text>
+      <Text>Mausritter is an easy to pick up game where you explore the world as mice adventurers. By using wacky items and strange magics to navigate the big world, there is a unique physical card-based inventory system to keep things limited, but fun.</Text>
     </>
   )
 }
@@ -36,7 +36,7 @@ const AboutMausritter = () => {
 const HowToUse = () => {
   return (
     <>
-      <Text>Critter Sheets aims to bridge the gap between not having the physical box copy by giving an alternative online way to play the game.</Text>
+      <Text>Critter Sheets aims to bridge the gap of not having the physical box copy by giving an alternative online way to play the game.</Text>
       <Text>By using drag-and-drop equipment slots and quality of life features like dice rolling, quick access to rules, and creating custom components on the fly, hopefully this is a useful tool for you!</Text>
     </>
   )
@@ -45,7 +45,7 @@ const HowToUse = () => {
 const Support = () => {
   return (
     <>
-      <Text>This is a personal project made by two friends so it will always remain free and open-source. We really enjoy Mausritter so we're hoping this can be a tool for new players to easily get into it.</Text>
+      <Text>This is a personal project so it will always remain free and open-source. We really enjoy Mausritter so we're hoping this can be a tool for players to easily get into it.</Text>
     </>
   )
 }
